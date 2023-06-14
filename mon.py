3ec9ea1bdbcd009e1ce88d6195bdf5f2dd1e9b52
CurvixSsH <127477293+CurvixSsH@users.noreply.github.com>
2023-06-14 00:57:28 -0600

    Add files via upload

---
+++ mon.py
@@ -0,0 +1,29 @@
+import subprocess
+
+def change_network_mode():
+    network_name = input("Ingresa el nombre de la red: ")
+
+    result = subprocess.run(["sudo", "ip", "link", "set", network_name, "down"], capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return
+
+    new_name = network_name + "mon"
+    result = subprocess.run(["sudo", "ip", "link", "set", network_name, "name", new_name], capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return
+
+    result = subprocess.run(["sudo", "ip", "link", "set", new_name, "up"], capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return
+
+    result = subprocess.run(["sudo", "iwconfig", new_name, "mode", "monitor"], capture_output=True, text=True)
+    if result.returncode != 0:
+        print(f"Error: {result.stderr}")
+        return
+
+    subprocess.run(["iwconfig"])
+
+change_network_mode()
